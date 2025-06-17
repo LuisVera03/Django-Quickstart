07326eaf56ec1bae0e709532e34bad9bd94577fd
Agustin Palopoli <agustinpalopoli1@gmail.com>
2025-06-17 13:43:51 -0300

    crud_form

---
+++ rest_basic/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from . import models
+
+class Table3Form(forms.ModelForm):
+    class Meta:
+        model = models.Table3
+        fields = '__all__'
+
+class Table2Form(forms.ModelForm):
+    class Meta:
+        model = models.Table2
+        fields = '__all__'
+
+class Table1Form(forms.ModelForm):
+    class Meta:
+        model = models.Table1
+        fields = '__all__'
+        widgets = {
+            'many_to_many': forms.CheckboxSelectMultiple,
+            'date_field': forms.DateInput(attrs={'type': 'date'}),
+            'time_field': forms.TimeInput(attrs={'type': 'time'}),
+            'datetime_field': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
--- rest_basic/urls.py
+++ rest_basic/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('update_data', views.update_data, name='update_data'),
     path('delete_data_1', views.delete_data_1, name='delete_data_1'),
     path('delete_data_2', views.delete_data_2, name='delete_data_2'),
+    path('crud_form', views.crud_form, name='crud_form'),
+    path('get_data_form', views.get_data_form, name='get_data_form'),
+    path('form/<str:table>', views.form, name='form'),
+    path('update_form/<str:table>/<int:id>', views.update_form, name='update_form'),
+    path('add_data_form', views.add_data_form, name='add_data_form'),
+    path('update_data_form', views.update_data_form, name='update_data_form'),
 ]
