54740d430d5848bbeca81dcd824991cf6e5026a8
Agus P <agustin@gmail.com>
2025-07-28 18:01:26 -0300

    fix of autologout app2

--- layer_and_generic/views.py
+++ layer_and_generic/views.py
@@ -58,32 +58,39 @@ class LogoutView(View):
         perform_logout(request)
         return redirect('login')
 
+# Para vistas basadas en función
+@login_required
 def home(request):
     return render(request, 'LAG/home.html')
 
-
+# Para vistas basadas en clase
+@method_decorator(login_required, name='dispatch')
 class Table1ListView(ListView):
     model = Table1
     template_name = 'list.html'
     context_object_name = 'table1_list'
 
+@method_decorator(login_required, name='dispatch')
 class Table1DetailView(DetailView):
     model = Table1
     template_name = 'detail.html'
     context_object_name = 'table1'
 
+@method_decorator(login_required, name='dispatch')
 class Table1CreateView(CreateView):
     model = Table1
     template_name = 'form.html'
     form_class = Table1Form
     success_url = reverse_lazy('list')
 
+@method_decorator(login_required, name='dispatch')
 class Table1UpdateView(UpdateView):
     model = Table1
     template_name = 'form.html'
     form_class = Table1Form
     success_url = reverse_lazy('list')
 
+@method_decorator(login_required, name='dispatch')
 class Table1DeleteView(DeleteView):
     model = Table1
     template_name = 'confirm_delete.html'
