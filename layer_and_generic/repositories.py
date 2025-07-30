7c165c45ddc56ab0258c61a750a5389cbe79b7f3
LuisDvlp03 <Luis.dvlp.03@gmail.com>
2025-07-29 22:16:54 -0300

    Layers solution

--- layer_and_generic/repositories.py
+++ layer_and_generic/repositories.py
@@ -1,11 +1,101 @@
 from django.contrib.auth.models import User
-
+from django.db import transaction
+from rest_basic.models import Table1, Table2, Table3
 
 # User
 def get_user_by_username(username):
     return User.objects.filter(username=username).first()
 
+@transaction.atomic
 def create_user(username, email, password):
     print("Creating user:", username)
     return User.objects.create_user(username=username, email=email, password=password)
 
+# Table1 repositories
+def get_all_table1():
+    return Table1.objects.select_related('foreign_key', 'one_to_one').prefetch_related('many_to_many')
+
+def get_table1_by_id(id):
+    try:
+        return Table1.objects.select_related('foreign_key', 'one_to_one').prefetch_related('many_to_many').get(pk=id)
+    except Table1.DoesNotExist:
+        return None
+
+@transaction.atomic
+def create_table1(data):
+    many_to_many_ids = data.pop('many_to_many', [])
+    instance = Table1.objects.create(**data)
+    if many_to_many_ids:
+        instance.many_to_many.set(many_to_many_ids)
+    return instance
+
+@transaction.atomic
+def update_table1(instance, data):
+    many_to_many_ids = data.pop('many_to_many', None)
+    for field, value in data.items():
+        setattr(instance, field, value)
+    instance.save()
+    if many_to_many_ids is not None:
+        instance.many_to_many.set(many_to_many_ids)
+    return instance
+
+@transaction.atomic
+def delete_table1(instance):
+    if instance.image_field:
+        instance.image_field.delete(save=False)
+    if instance.file_field:
+        instance.file_field.delete(save=False)
+    instance.delete()
+    return True
+
+# Table2 repositories
+def get_all_table2():
+    return Table2.objects.all()
+
+def get_table2_by_id(id):
+    try:
+        return Table2.objects.get(pk=id)
+    except Table2.DoesNotExist:
+        return None
+
+@transaction.atomic
+def create_table2(data):
+    return Table2.objects.create(**data)
+
+@transaction.atomic
+def update_table2(instance, data):
+    for field, value in data.items():
+        setattr(instance, field, value)
+    instance.save()
+    return instance
+
+@transaction.atomic
+def delete_table2(instance):
+    instance.delete()
+    return True
+
+# Table3 repositories
+def get_all_table3():
+    return Table3.objects.all()
+
+def get_table3_by_id(id):
+    try:
+        return Table3.objects.get(pk=id)
+    except Table3.DoesNotExist:
+        return None
+
+@transaction.atomic
+def create_table3(data):
+    return Table3.objects.create(**data)
+
+@transaction.atomic
+def update_table3(instance, data):
+    for field, value in data.items():
+        setattr(instance, field, value)
+    instance.save()
+    return instance
+
+@transaction.atomic
+def delete_table3(instance):
+    instance.delete()
+    return True
--- layer_and_generic/services.py
+++ layer_and_generic/services.py
@@ -1,7 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 
-# Import functions for data manipulation by users
-from .repositories import create_user, get_user_by_username
+# Import functions for data manipulation
+from .repositories import (
+    create_user, get_user_by_username, 
+    get_all_table1, get_table1_by_id, create_table1, update_table1, delete_table1,
+    get_all_table2, get_table2_by_id, create_table2, update_table2, delete_table2,
+    get_all_table3, get_table3_by_id, create_table3, update_table3, delete_table3
+)
 
 #
 ## Login / Register
@@ -25,3 +30,86 @@ def register_user(username, email, password, password_confirm):
 def perform_logout(request):
     logout(request)
 
+#
+## Table1 Services
+#
+
+def get_table1_list():
+    return get_all_table1()
+
+def get_table1_detail(id):
+    return get_table1_by_id(id)
+
+def create_table1_service(data):
+    processed_data = data.copy()
+    none_fields = ['foreign_key', 'one_to_one', 'integer_field', 'float_field', 
+                   'date_field', 'time_field', 'datetime_field']
+    
+    for field in none_fields:
+        if field in processed_data and processed_data[field] == '':
+            processed_data[field] = None
+    
+    return create_table1(processed_data)
+
+def update_table1_service(instance, data):
+    processed_data = data.copy()
+    none_fields = ['foreign_key', 'one_to_one', 'integer_field', 'float_field', 
+                   'date_field', 'time_field', 'datetime_field']
+    
+    for field in none_fields:
+        if field in processed_data and processed_data[field] == '':
+            processed_data[field] = None
+    
+    return update_table1(instance, processed_data)
+
+def delete_table1_service(instance):
+    return delete_table1(instance)
+
+#
+## Table2 Services
+#
+
+def get_table2_list():
+    return get_all_table2()
+
+def get_table2_detail(id):
+    return get_table2_by_id(id)
+
+def create_table2_service(data):
+    return create_table2(data)
+
+def update_table2_service(instance, data):
+    return update_table2(instance, data)
+
+def delete_table2_service(instance):
+    return delete_table2(instance)
+
+#
+## Table3 Services
+#
+
+def get_table3_list():
+    return get_all_table3()
+
+def get_table3_detail(id):
+    return get_table3_by_id(id)
+
+def create_table3_service(data):
+    return create_table3(data)
+
+def update_table3_service(instance, data):
+    return update_table3(instance, data)
+
+def delete_table3_service(instance):
+    return delete_table3(instance)
+
+#
+## Dashboard Data
+#
+
+def get_dashboard_data():
+    return {
+        'total_table1': get_table1_list().count(),
+        'total_table2': get_table2_list().count(),
+        'total_table3': get_table3_list().count(),
+    }
